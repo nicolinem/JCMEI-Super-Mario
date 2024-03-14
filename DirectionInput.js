64312ab012fc4c089437c9710f0cee786a5bbce3
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2024-03-14 18:44:17 +0100

    takes in mario in init, can trigger functions on keys

--- DirectionInput.js
+++ DirectionInput.js
@@ -3,36 +3,41 @@ class DirectionInput {
     this.heldDirections = [];
 
     this.map = {
-      "ArrowUp": "up",
-      "KeyW": "up",
-      "ArrowDown": "down",
-      "KeyS": "down",
-      "ArrowLeft": "left",
-      "KeyA": "left",
-      "ArrowRight": "right",
-      "KeyD": "right",
-    }
+      ArrowUp: "up",
+      KeyW: "up",
+      ArrowDown: "down",
+      KeyS: "down",
+      ArrowLeft: "left",
+      KeyA: "left",
+      ArrowRight: "right",
+      KeyD: "right",
+    };
   }
 
   get direction() {
     return this.heldDirections[0];
   }
 
-  init() {
-    document.addEventListener("keydown", e => {
+  init(mario) {
+    document.addEventListener("keydown", (e) => {
       const dir = this.map[e.code];
       if (dir && this.heldDirections.indexOf(dir) === -1) {
         this.heldDirections.unshift(dir);
       }
+
+      if (e.code === "KeyS") {
+        mario.transformToSuper();
+      }
+      if (e.code === "KeyN") {
+        mario.transformToNormal();
+      }
     });
-    document.addEventListener("keyup", e => {
+    document.addEventListener("keyup", (e) => {
       const dir = this.map[e.code];
       const index = this.heldDirections.indexOf(dir);
       if (index > -1) {
         this.heldDirections.splice(index, 1);
       }
-    })
-
+    });
   }
-
 }
